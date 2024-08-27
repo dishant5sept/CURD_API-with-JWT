beb5172ee014b5d8e033f557e70108ed804a075f
dishant5sept <104493446+dishant5sept@users.noreply.github.com>
2024-08-27 12:58:00 +0530

    Create userControllers.js

---
+++ src/controllers/userControllers.js
@@ -0,0 +1,23 @@
+const { User } = require('../models');
+
+exports.getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.deleteUser = async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    await user.destroy();
+    res.json({ message: 'User deleted successfully' });
+  } catch (error) {
+    next(error);
+  }
+};
